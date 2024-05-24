ed5247aff08d032a912a3b3d7a3adcacee9b9502
Mykola Krylevych <krilevickola@gmail.com>
2024-05-24 13:13:28 +0200

    first commit

---
+++ tailwind-default.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue,css}',
+  ],
+  content: ['./public/**/*.{html,js}',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue,css}',
+  ],
+  content: ['./public/**/*.{html,js}',],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#FF6363',
+        secondary: {
+          '100':'#E2E2D5',
+          '200':'#888883',
+      }
+    },
+      fontFamily:{
+        body: ['Nunito']
+      }
+      
+    },
+  },
+  plugins: [],
+}
